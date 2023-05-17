12fac37dbce9459fc47e909ed0db89f8f50b3980
Valera <valeraone@inbox.ru>
2023-05-17 16:03:23 +0300

    update

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,6 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
+
         fetch('http://192.168.0.2:8000/web-data', {
             method: 'POST',
             headers: {
